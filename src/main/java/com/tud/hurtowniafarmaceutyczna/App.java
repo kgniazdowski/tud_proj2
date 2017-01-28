282d31786dc3126a0064bab960e71ccf82ad5e1c
Karolina Sankowska <Karolina Sankowska>
2017-01-28 16:22:17 +0100

    Inicjacja projektu

---
+++ src/main/java/com/tud/hurtowniafarmaceutyczna/App.java
@@ -0,0 +1,13 @@
+package com.tud.hurtowniafarmaceutyczna;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
